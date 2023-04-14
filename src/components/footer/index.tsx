094dba83208dcd967d8a849fea6b62ed0cdbf39b
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-15 01:43:55 +0800

    feat: 底部导航条跳转

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ListItem from '../list-item';
 import styles from './index.module.scss';
+import { MailFill, MessageFill, SmileFill, StarFill } from 'antd-mobile-icons';
+import { useNavigate } from 'react-router-dom';
 
 export interface FooterProps {}
 
@@ -9,20 +11,53 @@ export interface FooterProps {}
  * @returns
  */
 function Header(props: FooterProps) {
-  return (
-    <div className={styles.empty_block}>
-      <div className={styles.footer}>
-        {['聊天', '物品', '地图', '我'].map((item, index) => {
-          return (
-            <div key={index}>
-              <img className={styles.img} src="/logo192.png" alt="icon" />
-              <div>{item}</div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+  interface Footer {
+    name: string;
+    icon: any;
+    path: string;
+  }
+	const footers: Footer[] = [
+		{
+			name: '聊天',
+			icon: <MessageFill fontSize={24} />,
+			path: '/chat',
+		},
+		{
+			name: '物品',
+			icon: <MailFill fontSize={24} />,
+			path: '/itemBars',
+		},
+		{
+			name: '地图',
+			icon: <StarFill fontSize={24} />,
+			path: '/chapters',
+		},
+		{
+			name: '我',
+			icon: <SmileFill fontSize={24} />,
+			path: '/charactersSelection',
+		},
+	];
+  const navigate = useNavigate();
+	const handleFooter = (path: string) => {
+    navigate(path)
+  };
+	return (
+		<div className={styles.empty_block}>
+			<div className={styles.footer}>
+				{footers.map((item, index) => {
+					return (
+						<div key={index}>
+							<div onClick={() => handleFooter(item.path)}>
+								<div className={styles.img}>{item.icon}</div>
+								<span>{item.name}</span>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default Header;
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,7 @@
 import React from 'react';
 import { lazy } from 'react';
+import type { RouteObject } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 const Home = lazy(() => import('../views/home'));
 const Book = lazy(() => import('../views/books'));
 const CharactersDetails = lazy(
@@ -13,8 +15,6 @@ const Storyline = lazy(() => import('../views/story/storyline'));
 const ItemBars = lazy(() => import('../views/item/itemBars'));
 const ItemDetails = lazy(() => import('../views/item/itemDetails'));
 const Chat = lazy(() => import('../views/chat'));
-import type { RouteObject } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
 
 export const routes: RouteObject[] = [
 	{
