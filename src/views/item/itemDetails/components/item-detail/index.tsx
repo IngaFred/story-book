9013c6b5c9bf1c2cb84964d6c0fd7ac6ed8b26fe
626723063 <626723063@qq.com>
2023-04-15 15:40:28 +0800

    fix: 命名修复

--- src/views/item/itemDetails/components/item-detail/index.tsx
+++ src/views/item/itemDetails/components/item-detail/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import styles from './index.module.scss';
 import { useLocation } from 'react-router-dom';
 import { Image } from 'antd-mobile';
-import type { items } from '../../../itemBars/components/item-list';
+import type { ItemProps } from '../../../itemBars/components/item-list';
 
 export default function ItemDetail() {
-    const location = useLocation();
-	const character: items = location?.state?.character || null;
-	return (
-		<div className={styles.all}>
-			<Image src={character?.urlDetail} fit="fill" />
-			<div className={styles.infos}>{character?.introduction}</div>
-		</div>
-	);
+  const location = useLocation();
+  const character: ItemProps = location?.state?.character || null;
+  return (
+    <div className={styles.all}>
+      <Image src={character?.urlDetail} fit="fill" />
+      <div className={styles.infos}>{character?.introduction}</div>
+    </div>
+  );
 }
