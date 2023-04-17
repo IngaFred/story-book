663f69ff2213587cb2906d7045b9fd89133c92cd
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-18 01:44:25 +0800

    fix: 物品详情修改

--- src/views/item/itemDetails/components/item-detail/index.tsx
+++ src/views/item/itemDetails/components/item-detail/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { useLocation } from 'react-router-dom';
-import { Image } from 'antd-mobile';
+import { Image, Card, Grid, Divider } from 'antd-mobile';
 import type { ItemProps } from '../../../itemBars/components/item-list';
 
 export default function ItemDetail() {
-  const location = useLocation();
-  const character: ItemProps = location?.state?.character || null;
-  return (
-    <div className={styles.all}>
-      <Image src={character?.urlDetail} fit="fill" />
-      <div className={styles.infos}>{character?.introduction}</div>
-    </div>
-  );
+	const location = useLocation();
+	const character: ItemProps = location?.state?.character || null;
+	return (
+		<div className={styles.all}>
+			<Grid columns={2} gap={16} className={styles.item}>
+				<div>
+					<Image src={character?.url} height={200} width={200} fit="fill" />
+				</div>
+				<div className={styles.infos}>{character?.introduction}</div>
+			</Grid>
+			<Divider
+				contentPosition="left"
+				style={{
+					color: 'black',
+          borderColor: 'black',
+					borderStyle: 'dashed',
+				}}
+			>
+				详情描述
+			</Divider>
+			<Grid columns={1} className={styles.itemDetail}>
+				<div>
+					<Image src={character?.urlDetail} fit="fill" />
+				</div>
+			</Grid>
+		</div>
+	);
 }
