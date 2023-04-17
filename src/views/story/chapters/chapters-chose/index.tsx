9b9eaa76f820bdaf10a279aae50797dc933be60f
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-18 05:27:15 +0800

    fix: Bug修复

--- src/views/story/chapters/chapters-chose/index.tsx
+++ src/views/story/chapters/chapters-chose/index.tsx
@@ -1,89 +1,94 @@
 import React from 'react';
 import { List } from 'antd-mobile';
-import styles from './index.module.scss';
 import { useNavigate } from 'react-router-dom';
+import styles from './index.module.scss';
 
 interface Item {
-	id: string;
-	name: string;
-	path: string;
-	children?: Item[];
+  id: string;
+  name: string;
+  path: string;
+  children?: Item[];
 }
 
 interface DirectoryProps {
-	data: Item;
+  data: Item;
 }
 
 function Chose() {
-	const data: Item[] = [
-		{
-			id: '1',
-			name: '故事一',
-			path: '/',
-			children: [
-				{
-					id: '01',
-					name: '银子那些事儿',
-					path: '',
-					children: [
-						{
-							id: '001',
-							name: '第一章',
-							path: '/home',
-						},
-						{
-							id: '002',
-							name: '第二章',
-							path: '/home',
-						},
-					],
-				},
-			],
-		},
-	];
+  const data: Item[] = [
+    {
+      id: '1',
+      name: '故事',
+      path: '/',
+      children: [
+        {
+          id: '01',
+          name: '银子那些事儿',
+          path: '',
+          children: [
+            {
+              id: '001',
+              name: '第一章',
+              path: '/home',
+            },
+            {
+              id: '002',
+              name: '第二章',
+              path: '/home',
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
-	return (
-		<div className={styles.all}>
-			{data.map((item, index) => {
-				return <Directory data={item} key={index} />;
-			})}
-		</div>
-	);
+  return (
+    <div className={styles.all}>
+      {data.map((item) => (
+        <Directory data={item} key={item.id} />
+      ))}
+    </div>
+  );
+}
+
+function ListItem(props: { item: Item }) {
+  const navigate = useNavigate();
+
+  const go = (path: string) => {
+    navigate(path);
+  };
+
+  const renderChildren = (children?: Item[]) => {
+    if (children) {
+      return children.map((child) => (
+        <Directory data={child} key={child.id} />
+      ));
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <List.Item key={props.item.id}>
+      <div
+        onClick={() => {
+          go(props.item.path);
+        }}
+        className={styles.listItem}
+      >
+        {props.item.name}
+      </div>
+      {renderChildren(props.item.children)}
+    </List.Item>
+  );
 }
 
 function Directory(props: DirectoryProps) {
-	const navigate = useNavigate();
-	const renderItem = (item: Item) => {
-		return <div style={{ padding: '0.3rem' }}>{item.name}</div>;
-	};
-	const go = (path: string) => {
-		navigate(path);
-	};
-
-	return (
-		<List>
-			{props.data.children?.map((item) => (
-				<div
-					style={{
-						fontWeight: 'bold',
-						backgroundColor: '#f5f5f5',
-						padding: '0.5rem',
-					}}
-				>
-					<List.Item key={item.id}>
-						<div
-							onClick={() => {
-								go(item.path);
-							}}
-						>
-							{renderItem(item)}
-						</div>
-						{item.children && <Directory data={item} />}
-					</List.Item>
-				</div>
-			))}
-		</List>
-	);
+  return (
+    <>
+      <ListItem item={props.data} />
+    </>
+  );
 }
 
 export default Chose;
