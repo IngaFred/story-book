9bab8cd523162f7fa5e1c0bf6ab5a3f8b7dcddc8
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-19 00:53:39 +0800

    feat: store修改，角色选择 我的角色查看

--- src/views/characters/charactersSelection/components/show-card/index.tsx
+++ src/views/characters/charactersSelection/components/show-card/index.tsx
@@ -1,94 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 import { Button, Grid, Image } from 'antd-mobile';
-import A from '@/assets/images/characters/布脱脱.jpg';
-
-import B from '@/assets/images/characters/廖不到.jpg';
-
-import C from '@/assets/images/characters/扈扈.jpg';
-
-import D from '@/assets/images/characters/胡软.jpg';
 import { useLocation, useNavigate } from 'react-router-dom';
-
+import { charactersList } from '@/data/chapter-chose';
+import { useChoseStore } from '@/store';
 export interface Characters {
-  name: string;
-  url: string;
-  introduction: string;
-  id?: string;
+	name: string;
+	url: string;
+	introduction: string;
+	id: string;
 }
-
-const charactersList: Characters[] = [
-  {
-    name: '布脱脱',
-    url: A,
-    introduction: '阳光开朗大男孩！',
-  },
-  {
-    name: '廖不到',
-    url: B,
-    introduction: '古灵多识怪爷爷！',
-  },
-  {
-    name: '扈扈',
-    url: C,
-    introduction: '可爱明朗小妹妹！',
-  },
-  {
-    name: '胡软',
-    url: D,
-    introduction: '孤高帅气冷傲娇！',
-  },
-];
 /**
  * 角色选择
  */
 function ShowCard() {
-  const navigate = useNavigate();
-  const [selected, setSelected] = useState<Boolean>(false);
-  const [choseCharacter, setChoseCharacter] = useState<Characters | undefined>(
-    undefined
-  );
-  const location = useLocation();
-  const { chose, isSelected } = location?.state || {};
-  useEffect(() => {
-    typeof isSelected === 'boolean' && setSelected(isSelected);
-    typeof chose === 'object' && setChoseCharacter(chose);
-  }, [chose, isSelected]);
+	const navigate = useNavigate();
+	const [selected, setSelected] = useState<Boolean>(false);
+	const [choseCharacter, setChoseCharacter] = useState<Characters | undefined>(
+		undefined
+	);
+	const location = useLocation();
+	const { chose, isSelected } = location?.state || {};
 
-  // console.log('isSelected ==>', isSelected);
-  // console.log('selected ==>', selected)
-  // console.log('chose', chose);
-  const handleFooter = (selectCharacter: Characters) => {
-    navigate('/charactersDetails', { state: { character: selectCharacter } });
-  };
-  const setStore = () => {
-    navigate('/chapters');
-  };
-  return (
-    <div className={styles.all}>
-      <Grid columns={2} gap={8}>
-        {charactersList.map((item, index) => {
-          return (
-            <div key={index}>
-              <div onClick={() => handleFooter(item)} className={styles.one}>
-                <Image src={item.url} fit="fill" />
-                <span className={styles.span}>{item.name}</span>
-                {/* <div className={styles.infos}>{item.introduction}</div> */}
-              </div>
-            </div>
-          );
-        })}
-      </Grid>
-      {isSelected ? (
-        <Button block color="primary" size="large" onClick={setStore}>
-          确认选择{choseCharacter?.name}
-        </Button>
-      ) : (
-        <Button block color="warning" size="large">
-          点击角色选择
-        </Button>
-      )}
-    </div>
-  );
+	useEffect(() => {
+		typeof isSelected === 'boolean' && setSelected(isSelected);
+		typeof chose === 'object' && setChoseCharacter(chose);
+	}, [chose, isSelected]);
+
+	const handleFooter = (selectCharacter: Characters) => {
+		navigate('/charactersDetails', { state: { character: selectCharacter } });
+	};
+
+	const setChoseChapter = useChoseStore((state) => state.setChoseChapter);
+	const setStore = ( chapter: Characters) => {
+		// 设置 chapterStore
+		setChoseChapter(chapter);
+		navigate('/chapters');
+	};
+
+	return (
+		<div className={styles.all}>
+			<Grid columns={2} gap={8}>
+				{charactersList.length > 0 &&
+					charactersList.map((item, index) => {
+						return (
+							<div key={index}>
+								<div  onClick={() => handleFooter(item)} className={styles.one}>
+									<Image src={item?.url} fit="fill" />
+									<span className={styles.span}>{item?.name}</span>
+									{/* <div className={styles.infos}>{item.introduction}</div> */}
+								</div>
+							</div>
+						);
+					})}
+			</Grid>
+			{isSelected ? (
+				<Button
+					block
+					color="primary"
+					size="large"
+					onClick={() => setStore(choseCharacter as Characters)}
+				>
+					确认选择{choseCharacter?.name}
+				</Button>
+			) : (
+				<Button block color="warning" size="large">
+					点击角色选择
+				</Button>
+			)}
+		</div>
+	);
 }
 export default ShowCard;
--- src/views/myself/components/detail/index.tsx
+++ src/views/myself/components/detail/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { useLocation } from 'react-router-dom';
-import { Button, Image } from 'antd-mobile';
+import { ErrorBlock, Image } from 'antd-mobile';
 import { Characters } from '@/views/characters/charactersSelection/components/show-card';
+import { useChoseStore } from '@/store';
 
 export default function Detail() {
-  const location = useLocation();
-  const character: Characters = location?.state?.character || null;
-  return (
-    <div className={styles.all}>
-      <Image src={character?.url} fit="fill" />
-      <div className={styles.infos}>{character?.name}</div>
-      <div className={styles.infos}>{character?.introduction}</div>
-    </div>
-  );
+	// const location = useLocation();
+	// const character: Characters = location?.state?.character ?? null;
+	const character: Characters | undefined =
+		useChoseStore((state) => state.chapter) ?? undefined;
+	return (
+		<div className={styles.all}>
+			{typeof character === 'object' ? (
+				<>
+					<Image src={character?.url} fit="fill" />
+					<div className={styles.infos}>{character?.name}</div>
+					<div className={styles.infos}>{character?.introduction}</div>
+				</>
+			) : (
+				<ErrorBlock description="请正确选择角色！" />
+			)}
+		</div>
+	);
 }
