6881d46a47434c6f3522c3e43e0d4a8d625e2813
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-04-18 14:04:01 +0800

    fix: 修改store

--- src/data/chapter-chose.ts
+++ src/data/chapter-chose.ts
@@ -1,7 +1,35 @@
 import { Characters } from '@views/characters/charactersSelection/components/show-card';
+import A from '@/assets/images/characters/布脱脱.jpg';
 
-export const characterChose: Characters = {
-	name: '布脱脱',
-	url: '',
-	introduction: '阳光开朗大男孩！',
-};
+import B from '@/assets/images/characters/廖不到.jpg';
+
+import C from '@/assets/images/characters/扈扈.jpg';
+
+import D from '@/assets/images/characters/胡软.jpg';
+
+export const charactersList: Characters[] = [
+	{
+		name: '布脱脱',
+		url: A,
+		introduction: '阳光开朗大男孩！',
+		id: '01',
+	},
+	{
+		name: '廖不到',
+		url: B,
+		introduction: '古灵多识怪爷爷！',
+		id: '02',
+	},
+	{
+		name: '扈扈',
+		url: C,
+		introduction: '可爱明朗小妹妹！',
+		id: '03',
+	},
+	{
+		name: '胡软',
+		url: D,
+		introduction: '孤高帅气冷傲娇！',
+		id: '04',
+	},
+];
